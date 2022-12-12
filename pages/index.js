a5c5e065a89487bb5f049358df7c3a61f6bf0e0b
MohamedMassoud10 <massoudfcai@gmail.com>
2022-12-12 16:40:03 +0200

    try to deploy

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React, { Fragment } from "react";
 import Hero from "./component/UI/Hero";
 import Services from "./component/UI/Services";
 import "remixicon/fonts/remixicon.css";
-import About from "./component/UI/about";
 const Home = () => {
   return (
     <Fragment>
