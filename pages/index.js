2d37ad42c43cb6248f5bce87a9f67d76b85ea59b
MohamedMassoud10 <massoudfcai@gmail.com>
2022-12-12 16:45:00 +0200

    i think the deploy ment is succssed

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import Hero from "./component/UI/Hero";
 import Services from "./component/UI/Services";
 import "remixicon/fonts/remixicon.css";
+import About from "../pages/component/UI/About";
 const Home = () => {
   return (
     <Fragment>
