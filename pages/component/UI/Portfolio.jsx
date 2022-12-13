a0d3093414fb18de4c11aa085d68a5d1ae093ef2
MohamedMassoud10 <massoudfcai@gmail.com>
2022-12-13 17:39:54 +0200

    yarb teshta8al b2aa ):

--- pages/component/UI/Portfolio.jsx
+++ pages/component/UI/Portfolio.jsx
@@ -2,62 +2,8 @@ import React from "react";
 import SectionSubtitle from "./SectionSubtitle";
 import classes from "../../../styles/portfolio.module.css";
 import PortfolioItem from "./PortfolioItem";
+import portfolio from "../data/portfolio";
 export default function Portfolio() {
-  const portfolio = [
-    {
-      title: "Portfolio",
-      img: "/images/portfolio.png",
-      category: "MyProjects",
-      keyword: ["HTML", "CSS", "JS", "Next.js"],
-      liveUrl: "https://massoud-one.vercel.app/",
-    },
-    {
-      title: "E-Commerce Website",
-      img: "/images/E-commerce.png",
-      category: "MyProjects",
-      keyword: ["HTML", "CSS", "JS", "React.JS"],
-      liveUrl: "https://sneakerssite.onrender.com/",
-    },
-
-    {
-      title: "Landing page",
-      img: "/images/landingPage.png",
-      category: "MyProjects",
-      keyword: ["HTML", "CSS", "JS", "React.JS"],
-      liveUrl: "https://kasper-16um.onrender.com/",
-    },
-
-    {
-      title: "Quiz Application",
-      img: "/images/QuizaApp.png",
-      category: "MyProjects",
-      keyword: ["HTML", "CSS", "JS"],
-      liveUrl: "https://quiz-app-using-react.onrender.com/",
-    },
-
-    {
-      title: "Note Application",
-      img: "/images/NoteAPP.png",
-      category: "MyProjects",
-      keyword: ["HTML", "CSS", "JS"],
-      liveUrl: "https://react-note-app-z720.onrender.com/",
-    },
-    {
-      title: "to do list Application",
-      img: "/images/ToDOListAPP.png",
-      category: "MyProjects",
-      keyword: ["HTML", "CSS", "JS"],
-      liveUrl: "https://mohamedmassoud10.github.io/to-do-list",
-    },
-
-    {
-      title: "Tic Tac Toe Game",
-      img: "/images/tictactoe.png",
-      category: "MyProjects",
-      keyword: ["HTML", "CSS", "JS"],
-      liveUrl: "https://mohamedmassoud10.github.io/tic-tac-toe-game/",
-    },
-  ];
   return (
     <div id="portfolio">
       <div className={`${classes.container}`}>
---
+++ pages/component/data/portfolio.js
@@ -0,0 +1,57 @@
+const portfolio = [
+  {
+    title: "Portfolio",
+    img: "/images/portfolio.png",
+    category: "MyProjects",
+    keyword: ["HTML", "CSS", "JS", "Next.js"],
+    liveUrl: "https://massoud-one.vercel.app/",
+  },
+  {
+    title: "E-Commerce Website",
+    img: "/images/E-commerce.png",
+    category: "MyProjects",
+    keyword: ["HTML", "CSS", "JS", "React.JS"],
+    liveUrl: "https://sneakerssite.onrender.com/",
+  },
+
+  {
+    title: "Landing page",
+    img: "/images/landingPage.png",
+    category: "MyProjects",
+    keyword: ["HTML", "CSS", "JS", "React.JS"],
+    liveUrl: "https://kasper-16um.onrender.com/",
+  },
+
+  {
+    title: "Quiz Application",
+    img: "/images/QuizaApp.png",
+    category: "MyProjects",
+    keyword: ["HTML", "CSS", "JS"],
+    liveUrl: "https://quiz-app-using-react.onrender.com/",
+  },
+
+  {
+    title: "Note Application",
+    img: "/images/NoteAPP.png",
+    category: "MyProjects",
+    keyword: ["HTML", "CSS", "JS"],
+    liveUrl: "https://react-note-app-z720.onrender.com/",
+  },
+  {
+    title: "to do list Application",
+    img: "/images/ToDOListAPP.png",
+    category: "MyProjects",
+    keyword: ["HTML", "CSS", "JS"],
+    liveUrl: "https://mohamedmassoud10.github.io/to-do-list",
+  },
+
+  {
+    title: "Tic Tac Toe Game",
+    img: "/images/tictactoe.png",
+    category: "MyProjects",
+    keyword: ["HTML", "CSS", "JS"],
+    liveUrl: "https://mohamedmassoud10.github.io/tic-tac-toe-game/",
+  },
+];
+
+export default portfolio;
