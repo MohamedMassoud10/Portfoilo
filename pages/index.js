25031a4559c86eb633d7b707bdd11449f7f395a4
MohamedMassoud10 <massoudfcai@gmail.com>
2024-08-24 19:34:41 +0300

    updated5

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Header from "../pages/component/Header/Header";
 import Footer from "../pages/component/Footer/Footer";
 import Portfolio from "./component/UI/Portfolio";
 import Contact from "./component/UI/Contact";
+import PortfolioImage from "../public/images/potfolio.jpg";
 import { Helmet } from "react-helmet";
 const Home = () => {
   <Helmet>
@@ -14,7 +15,27 @@ const Home = () => {
       name="description"
       content="Mohamed Massoud Aspiring Software engineer"
     />
-    <meta name="keywords" content="Mohamed Massoud" />
+    <meta name="keywords" content="Mohamed Massoud" />{" "}
+    <meta property="og:title" content={title} />
+    <meta
+      property="og:description"
+      content={"Mohamed Massoud Aspiring Software engineer"}
+    />
+    <meta property="og:image" content={PortfolioImage} />
+    <meta property="og:url" content={"https://massoud-one.vercel.app"} />
+    <meta
+      property="og:type"
+      content=" I am a self-motivated Full Stack Web Developer with a strong drive
+              to learn and explore the latest technologies to create
+              high-performance applications. I excel in both independent work
+              and team collaborations. Skilled in front-end technologies like
+              HTML5, CSS3, SASS, JavaScript, React, React Hooks, Redux, and
+              Next.js, as well as back-end development with Node.js, Express.js,
+              and MongoDB. I am proficient in building and managing RESTful APIs
+              and working with the full MERN stack. With excellent communication
+              skills, I ensure top-quality service to meet and exceed customer
+              expectations."
+    />
   </Helmet>;
   return (
     <Fragment>
